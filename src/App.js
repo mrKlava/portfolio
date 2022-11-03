bc3a5cef02f25ee3319102b85cbfda78ed94664f
mrKlava <ArtursKlava@yandex.com>
2022-11-03 22:12:30 +0100

    added ui component Title with two types: Section - default and Card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Title } from "./ui";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <Title text='About me' type='section' />
+      </div>
     </div>
   );
 }
---
+++ src/ui/title/Title.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './title.scss';
+
+/* USING POSITION - ABSOLUTE */
+
+function Title({text, type='section'}) {
+  return (
+    type==='card' 
+    ?
+    <div className='title-container --card'>
+        <div className='title-inner'>
+            <h4 className='title --blue --card'>{text}</h4>
+            <h4 className='title --pink --card'>{text}</h4>
+            <h4 className='title --white'>{text}</h4>
+        </div>
+    </div>
+    : 
+    <div className='title-container'>
+        <div className='title-inner'>
+            <h2 className='title --pink'>{text}</h2>
+            <h2 className='title --blue'>{text}</h2>
+            <h2 className='title --white'>{text}</h2>
+        </div>
+    </div>
+  )
+}
+// function TitleSection({text}) {
+//   return (
+//     <div className='title-container'>
+//         <div className='title-inner'>
+//             <h2 className='title --pink'>{text}</h2>
+//             <h2 className='title --blue'>{text}</h2>
+//             <h2 className='title--white'>{text}</h2>
+//         </div>
+//     </div>
+//   )
+// }
+
+export default Title
+
+
+/* USING BOX-SHADOW */
+
+// function TitleSection({text}) {
+//   return (
+//     <div className='title-container'>
+//         <h2 className='title'>{text}</h2>
+//     </div>
+//   )
+// }
+
+// export default TitleSection
